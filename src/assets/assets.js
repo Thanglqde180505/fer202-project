368386e4790fc84cd04805fed644f9ebae4414df
Liamm-shkerpear <trungthanhle2004@gmail.com>
2024-11-04 17:42:01 +0700

    add img and vid

---
+++ src/assets/assets.js
@@ -0,0 +1,29 @@
+import avatar from './img/avatar.jpg';
+import banner_contact from './img/contact-banner.avif';
+import banner_about from './img/jh-about.webp';
+import logo from './img/logo.png';
+import quantity1 from './img/quantity-web/quantity1.jpg';
+import quantity2 from './img/quantity-web/quantity2.jpeg';
+import quantity3 from './img/quantity-web/quantity3.png';
+import star_dull from './img/star_dull_icon.png';
+import star from './img/star_icon.png';
+// import video from './video/video-intro.mp4';
+export const assets = {
+    logo,
+    banner_about,
+    banner_contact,
+    quantity1,
+    quantity2,
+    quantity3,
+    star,
+    star_dull,
+    avatar
+}
+
+
+export const adminAccount = {
+    id: '1',
+    userName: 'admin',
+    passWord: '123',
+    avatar: [avatar]
+}
