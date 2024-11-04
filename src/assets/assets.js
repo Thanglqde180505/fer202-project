b6beee68a2f01518ac5c0019ef34f0c261e47998
Liamm-shkerpear <trungthanhle2004@gmail.com>
2024-11-04 18:40:03 +0700

    import video

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -7,9 +7,10 @@ import quantity2 from './img/quantity-web/quantity2.jpeg';
 import quantity3 from './img/quantity-web/quantity3.png';
 import star_dull from './img/star_dull_icon.png';
 import star from './img/star_icon.png';
-// import video from './video/video-intro.mp4';
+import video from './video/video-intro.mp4';
 export const assets = {
     logo,
+    video,
     banner_about,
     banner_contact,
     quantity1,
